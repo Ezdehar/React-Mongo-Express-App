98611656d57355edde49b0dd32a74d8f0a392e38
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-07-08 17:20:10 -0400

    update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloReact = () => (
+  <p>Hello World!</p>
+);
+
+ReactDOM.render(<HelloReact />, document.getElementById("root"));
