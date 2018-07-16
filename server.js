a6e876d6c04b8a1165da89c1ef883f1d915cfb83
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-07-16 15:25:57 -0400

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/nytarticles",
   {
-    useNewUrlParser: true
+    useMongoClient: true
   }
 );
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-const HelloReact = () => (
-  <p>Hello World!</p>
-);
-
-ReactDOM.render(<HelloReact />, document.getElementById("root"));
+ReactDOM.render( < App / > , document.getElementById("root"));
+registerServiceWorker();
